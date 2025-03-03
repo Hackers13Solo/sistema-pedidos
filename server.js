7a59fc9bcf1f91e3fa2171b6b862e4e6fdae0cd0
Hackers13Solo <lamafiamods2@gmail.com>
2025-03-03 10:30:28 -0300

    Atualizei o layout e adicionei o botão de enviar e-mail

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const cors = require('cors'); // Para permitir requisições do frontend
 const app = express();
 const port = 3000;
 
-// Configuração para servir arquivos estáticos
-app.use(express.static('public'));
-
+app.use(cors());
 app.use(bodyParser.json());
-
-let pedidos = [];
 
 // Configuração do Nodemailer
 const transporter = nodemailer.createTransport({
@@ -20,41 +17,34 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-// Rota para adicionar pedidos
-app.post('/adicionarPedido', (req, res) => {
+// Rota para enviar e-mail
+app.post('/enviar-email', (req, res) => {
     const { nomePedido, dataExpiracao } = req.body;
-    const novoPedido = { nomePedido, dataExpiracao };
-    pedidos.push(novoPedido);
 
-    // Verificar se a data de expiração está próxima
-    const dataExpiracaoObj = new Date(dataExpiracao);
+    const mailOptions = {
+        from: 'gestaopedidosambientenobre@gmail.com', // E-mail remetente
+        to: 'alexcrystian223@gmail.com', // E-mail destinatário
+        subject: `Detalhes do Pedido: ${nomePedido}`,
+        text: `O pedido "${nomePedido}" expira em ${dataExpiracao}. Faltam ${calcularDiasRestantes(dataExpiracao)} dias.`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send('Erro ao enviar e-mail');
+        } else {
+            console.log('E-mail enviado: ' + info.response);
+            res.status(200).send('E-mail enviado com sucesso');
+        }
+    });
+});
+
+function calcularDiasRestantes(dataExpiracao) {
     const hoje = new Date();
-    const diferencaDias = Math.ceil((dataExpiracaoObj - hoje) / (1000 * 60 * 60 * 24));
-
-    if (diferencaDias <= 2) {
-        const mailOptions = {
-            from: 'gestaopedidosambientenobre@gmail.com',
-            to: 'alexcrystian223@gmail.com',
-            subject: 'Alerta de Pedido Próximo da Expiração',
-            text: `O pedido "${nomePedido}" expira em ${dataExpiracao}. Faltam ${diferencaDias} dias.`
-        };
-
-        transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('E-mail enviado: ' + info.response);
-            }
-        });
-    }
-
-    res.json({ success: true });
-});
-
-// Rota para listar pedidos
-app.get('/pedidos', (req, res) => {
-    res.json(pedidos);
-});
+    const expiracao = new Date(dataExpiracao);
+    const diferenca = expiracao - hoje;
+    return Math.ceil(diferenca / (1000 * 60 * 60 * 24));
+}
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
